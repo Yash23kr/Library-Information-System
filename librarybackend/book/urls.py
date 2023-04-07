7fc6c4a36b1dd29e26c123b62f877efeaf8eb269
SAMPREETH-R-S <sampreeth2003@gmail.com>
2023-04-07 18:11:05 +0530

    changed the login and registration links

--- librarybackend/book/urls.py
+++ librarybackend/book/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('',views.home_page, name='home'),
+    path('login',views.login_page, name='login'),
+    path('registration',views.registration_page, name='registration'),
 ]
--- librarybackend/book/views.py
+++ librarybackend/book/views.py
@@ -5,3 +5,8 @@ from .models import *
 
 def home_page(request):
     return render(request, 'home_page.html')
+
+def login_page(request):
+    return render(request, 'login_page.html')
+def registration_page(request):
+    return render(request, 'registration_page.html')
